291a2491cb8fc5735667117b221210e22185294b
Gabriel Majeri <gabriel.majeri6@gmail.com>
2026-09-16 19:42:38 +0000

    Add script to plot and compare various preconditioned kernel matrix conditioning number estimation methods

---
+++ scripts/preconditioned_kernel_matrix_conditioning_number_evolution.py
@@ -0,0 +1,222 @@
+from pathlib import Path
+from time import perf_counter
+from typing import Annotated
+
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+import typer
+from pydantic import BaseModel
+from tqdm import tqdm
+
+from adaptive_rank.datasets.utils import load_dataset
+from adaptive_rank.interface import NumPyArrayAdapter
+from adaptive_rank.kernels import rbf_kernel
+from adaptive_rank.preconditioner import GreedilyPivotedCholeskyPreconditioner
+
+
+class KernelMatrixConditioningNumberEstimates(BaseModel):
+    ranks: list[int]
+    "Ranks of the greedily pivoted Cholesky decomposition used to precondition the kernel matrix."
+
+    eigenvalue_estimates: list[float]
+    "Estimates of the conditioning number of the preconditioned kernel matrix, computed based on the largest eigenvalue of the remainder matrix."
+
+    pivot_estimates: list[float]
+    "Estimates of the conditioning number of the preconditioned kernel matrix, computed based on the latest pivot of the greedily pivoted Cholesky decomposition."
+
+    scaled_pivot_estimates: list[float]
+    "Estimates of the conditioning number of the preconditioned kernel matrix, computed based on the latest pivot of the greedily pivoted Cholesky decomposition, multiplied by the dimension of the system matrix."
+
+    trace_estimates: list[float]
+    "Estimates of the conditioning number of the preconditioned kernel matrix, computed based on the trace of the remainder matrix."
+
+    normalized_trace_estimates: list[float]
+    "Estimates of the conditioning number of the preconditioned kernel matrix, computed based on the trace of the remainder matrix, normalized by the system dimension."
+
+
+def main(
+    dataset: Annotated[
+        str, typer.Option(help="Identifier of dataset to use.")
+    ] = "random-multivariate-normal",
+    num_points: Annotated[
+        int | None,
+        typer.Option(
+            "--num-points",
+            "-N",
+            help="Number of points of the dataset. Equal to the dimension of the kernel matrix.",
+        ),
+    ] = 1000,
+    dimension: Annotated[
+        int | None,
+        typer.Option(
+            "--dimension",
+            "-D",
+            help="Dimension of each point's feature vector. Only relevant for synthetic datasets.",
+        ),
+    ] = 16,
+    seed: Annotated[
+        int | None, typer.Option(help="Seed for random number generator")
+    ] = 42,
+    kernel_matrix_regularization_factor: Annotated[
+        float,
+        typer.Option(
+            help="Regularization factor for the kernel matrix. Added to the diagonal of the kernel matrix to ensure positive definiteness and improve numerical stability."
+        ),
+    ] = 1e-5,
+    preconditioner_regularization_factor: Annotated[
+        float,
+        typer.Option(
+            help="Regularization factor for the preconditioner. Added to the diagonal of the preconditioner matrix to ensure positive definiteness and improve numerical stability."
+        ),
+    ] = 1e-5,
+    estimate_computation_step: Annotated[
+        int,
+        typer.Option(
+            help="Step size for computing conditioning number estimates. Estimates are computed every 'estimate_computation_step' iterations of the preconditioner update."
+        ),
+    ] = 20,
+) -> None:
+    """Estimates the conditioning number of a regularized kernel matrix
+    as a function of the rank of the greedily pivoted Cholesky decomposition
+    used to precondition it.
+    """
+
+    print(f"Loading dataset '{dataset}'...")
+    labeled_dataset = load_dataset(dataset, num_points, 1.0, 0.0, dimension, seed)
+
+    points = labeled_dataset.X_train
+
+    N: int = points.shape[0]
+    D: int = points.shape[1]
+    print(f"Dataset loaded. Using N = {N} vectors, each of dimension D = {D}")
+
+    print("Constructing kernel matrix using NumPy...")
+    start_time = perf_counter()
+    K = rbf_kernel(points, points) + kernel_matrix_regularization_factor * np.eye(
+        N, dtype=np.float64
+    )
+    K_adapted = NumPyArrayAdapter(K)
+    end_time = perf_counter()
+    duration = end_time - start_time
+    print(f"Kernel matrix constructed in {duration:.4g} seconds.")
+
+    print("Starting to construct greedily pivoted Cholesky preconditioner...")
+    preconditioner = GreedilyPivotedCholeskyPreconditioner(
+        K_adapted,
+        max_rank=N,
+        regularization_factor=preconditioner_regularization_factor,
+    )
+
+    ranks: list[int] = []
+    eigenvalue_estimates: list[float] = []
+    pivot_estimates: list[float] = []
+    scaled_pivot_estimates: list[float] = []
+    trace_estimates: list[float] = []
+    normalized_trace_estimates: list[float] = []
+
+    print("Computing conditioning number estimates as we go...")
+
+    for rank in tqdm(range(N)):
+        preconditioner.update_inner()
+
+        if rank % estimate_computation_step == 0:
+            ranks.append(rank)
+
+            U = preconditioner._preconditioner_upper[: rank + 1, :]
+            remainder_matrix = K - U.T @ U
+            largest_eigenvalue = scipy.linalg.eigh(
+                remainder_matrix, eigvals_only=True, subset_by_index=[N - 1, N - 1]
+            )[0]
+            eigenvalue_estimates.append(
+                1 + largest_eigenvalue / kernel_matrix_regularization_factor
+            )
+
+            # 1 + d_k / mu
+            pivot_estimates.append(
+                1 + preconditioner.latest_pivot / kernel_matrix_regularization_factor
+            )
+
+            # 1 + N d_k / mu
+            scaled_pivot_estimates.append(
+                1
+                + N * preconditioner.latest_pivot / kernel_matrix_regularization_factor
+            )
+
+            # 1 + sum(d_i) / mu
+            trace_estimates.append(
+                1
+                + np.sum(preconditioner._matrix_diagonal)
+                / kernel_matrix_regularization_factor
+            )
+
+            # 1 + sum(d_i) / (N * mu)
+            normalized_trace_estimates.append(
+                1
+                + np.sum(preconditioner._matrix_diagonal)
+                / (N * kernel_matrix_regularization_factor)
+            )
+
+    print("Saving results to disk...")
+    results_directory = (
+        Path("results/preconditioned_kernel_matrix_conditioning_number_estimates")
+        / dataset
+    )
+    results_directory.mkdir(parents=True, exist_ok=True)
+
+    results = KernelMatrixConditioningNumberEstimates(
+        ranks=ranks,
+        eigenvalue_estimates=eigenvalue_estimates,
+        pivot_estimates=pivot_estimates,
+        scaled_pivot_estimates=scaled_pivot_estimates,
+        trace_estimates=trace_estimates,
+        normalized_trace_estimates=normalized_trace_estimates,
+    )
+
+    with open(results_directory / f"N_{N}.json", "w") as f:
+        f.write(results.model_dump_json(indent=2))
+
+    print("Plotting results")
+    plots_directory = (
+        Path("plots/preconditioned_kernel_matrix_conditioning_number_estimates")
+        / dataset
+    )
+    plots_directory.mkdir(parents=True, exist_ok=True)
+
+    fig, ax = plt.subplots()
+
+    fig.suptitle(
+        "Conditioning number estimates for kernel matrix\n"
+        + f"Dataset: {dataset}, N = {N}"
+    )
+
+    ax.plot(
+        ranks,
+        eigenvalue_estimates,
+        label="Eigenvalue-derived bound $1 + \\lambda_{max}(R_k) / \\mu$",
+    )
+    ax.plot(ranks, pivot_estimates, label="Pivot estimate $1 + d_k / \\mu$")
+    ax.plot(
+        ranks, scaled_pivot_estimates, label="Scaled pivot estimate $1 + N d_k / \\mu$"
+    )
+    ax.plot(ranks, trace_estimates, label="Trace estimate $1 + tr(R_k)/ \\mu$")
+    ax.plot(
+        ranks,
+        normalized_trace_estimates,
+        label="Normalized trace estimate $1 + tr(R_k) / (N \\mu)$",
+    )
+
+    ax.set_yscale("log")
+
+    ax.set_xlabel("Rank")
+    ax.set_ylabel("Conditioning number estimate")
+
+    ax.grid()
+    ax.legend()
+
+    fig.tight_layout()
+    fig.savefig(plots_directory / f"N_{N}.pdf")
+
+
+if __name__ == "__main__":
+    typer.run(main)
